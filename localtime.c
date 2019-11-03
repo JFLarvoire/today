1221be23392345e378d8cfc258fbecf61f2b1243
JFLarvoire <jf.larvoire@free.fr>
2019-11-03 12:29:00 +0100

    Added option -f to display the full date/time.

--- localtime.c
+++ localtime.c
@@ -10,9 +10,10 @@
 ** History:
 **   2019-01-20 JFL Created this program.
 **   2019-11-01 JFL Added support for dates in the ISO 8601 YYYY-DDD format.
+**   2019-11-03 JFL Added option -f to display the full date/time.
 */
 
-#define VERSION "2019-11-01"
+#define VERSION "2019-11-03"
 
 #include <stdio.h>
 #include <string.h>
@@ -22,6 +23,9 @@
 #include "include/debugm.h"
 
 #define streq(s1, s2) (!strcmp(s1, s2))
+
+#define FALSE 0
+#define TRUE 1
 
 int debug = 0;
 
@@ -33,8 +37,7 @@ Usage: localtime [OPTIONS] [DATE_TIME]\n\
 \n\
 Options:\n\
   -?            Display this help screen\n\
-  -N[:M]        Display time N hours and M minutes before sunrise\n\
-  +N[:M]        Display time N hours and M minutes after sunrise\n\
+  -f            Display the full date/time in the canonic ISO 8601 format\n\
   -V|--version  Display the program version\n\
 \n\
 Date_time: [YYYY-MM-DD][T]HH:MM[:SS][Z], default: now, default date: today\n\
@@ -49,6 +52,7 @@ int main(int argc, char *argv[]) {
   struct tm stm;
   struct tm *ptm = NULL;
   int iErr;
+  int iFull = FALSE;
 
   for (i=1; i<argc; i++) {
     char *arg = argv[i];
@@ -62,6 +66,11 @@ int main(int argc, char *argv[]) {
     if (   streq(arg, "-d")	/* -d = Debug mode */
         || streq(arg, "--debug")) {
       debug = 1;
+      continue;
+    }
+    if (   streq(arg, "-f")	/* -f = Full date/time mode */
+        || streq(arg, "--full")) {
+      iFull = 1;
       continue;
     }
     if (   streq(arg, "-V")     /* -V: Display the version */
@@ -86,6 +95,8 @@ int main(int argc, char *argv[]) {
     ptm = localtime(&now);	/* get ptr to gmt time struct */
   }
   
+  if (iFull) printf("%04d-%02d-%02d ", ptm->tm_year+1900, ptm->tm_mon+1, ptm->tm_mday);
+
   if (ptm->tm_min == -2) ptm->tm_min = 0;
   printf("%02d:%02d", ptm->tm_hour, ptm->tm_min);
   if (ptm->tm_sec == -2) {
